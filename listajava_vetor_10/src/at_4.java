09263c31555160e1bf3f20468aa9496b7576b439
JOAO VITOR RODRIGUES DOS SANTOS <joaords202@gmail.com>
2026-05-08 14:26:00 -0300

    Refactor at_4.java for clarity and simplicity

--- listajava_vetor_10/src/at_4.java
+++ listajava_vetor_10/src/at_4.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 public class at_4 {   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Insira 10 nomes e diremos a ordem inversa dele");
-    int num=10;
-    String[] vert=new String[num];
-    for (int i=0;i<num;i++){
+    String[] vert=new String[10];
+    for (int i=0;i<10;i++){
         vert[i]=sc.next();
-        if (i+1==num){
+        if (i+1==10){
             System.out.println("A ordem inversa dos nomes é:");
             for (;i>=0;i--){
                 System.out.println(vert[i]);
