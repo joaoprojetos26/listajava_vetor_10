f3fb6d7ec55f0c7f06caeabd7361c2b1b23de641
JOAO VITOR RODRIGUES DOS SANTOS <joaords202@gmail.com>
2026-05-08 14:14:24 -0300

    Update at_1.java

--- listajava_vetor_10/src/at_1.java
+++ listajava_vetor_10/src/at_1.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
 public class at_1 {
+   public class at_1 {
     static void main() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("por favor digite a quantidade de vezes que quer digitar");
-        int num = sc.nextInt();
+        System.out.println("digite um numero por favor");
         double soma = 0;
-        int[] vert = new int[num];
-        for (int i = 0; i < num; i++) {
-            System.out.println("digite um numero");
+        int[] vert = new int[10];
+        for (int i = 0; i < 10; i++) {
             vert[i]=sc.nextInt();
             soma=vert[i]+soma;
+            System.out.println("digite novamente");
         }
-        System.out.println("a soma total e: " +soma/num);
+        System.out.println("a soma total e: " +soma/10);
         sc.close();
     }
+}
 }
