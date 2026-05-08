ec242e90c6172d9fb976ac025d008595202f3450
JOAO VITOR RODRIGUES DOS SANTOS <joaords202@gmail.com>
2026-05-08 13:58:21 -0300

    Refactor at_7.java to simplify array initialization

--- listajava_vetor_10/src/at_7.java
+++ listajava_vetor_10/src/at_7.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 public class at_7 {
     static void main() {
         Scanner sc = new Scanner(System.in);
-        int gbt=10;
-        int aluno=10;
         int cont=0;
-        String [] vert2 = new String[aluno];
-        String [] vert = new String[gbt];
+        String [] vert2 = new String[10];
+        String [] vert = new String[10];
         int i=0;
         System.out.println("professor digite aqui as respostas corretas");
-        for (i=0;i<gbt;i++){
+        for (i=0;i<10;i++){
             vert[i]=sc.next();
             System.out.println("digite novamente");
         }
         System.out.println("agora digite as respostas do seu aluno");
-        for (i=0;i<gbt;i++){
+        for (i=0;i<10;i++){
             vert2[i]=sc.next();
             System.out.println("digite novamente");
            if (vert[i].equals(vert2[i]))
